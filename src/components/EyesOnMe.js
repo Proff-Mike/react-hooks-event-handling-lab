8f800a0654aef37b5835056ad1d40f4ffd69ce9b
Michael Okoe <michael.okoejr@gmail.com>
2022-07-29 08:09:52 +0000

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
+import Keypad from "./Keypad"
+
 // Code EyesOnMe Component Here
+function EyesOnMe()   {
+  function controlFocus  ()  {
+    console.log('Good!')
+  }
+  function controlBlur() {
+    console.log("Hey! Eyes on me!")
+  }
+
+  return(
+    <div>
+      <button onFocus={controlFocus} onBlur={controlBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function controlChange() {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={controlChange}/>
+        </div>
     )
 }
 
